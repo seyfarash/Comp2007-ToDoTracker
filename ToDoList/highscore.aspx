e54019151c15d6b14c63187e50cd27e037f0ef08
seyfarash <seyfarash@gmail.com>
2015-07-28 20:22:17 -0400

    Fix attempt #4

--- ToDoList/highscore.aspx
+++ ToDoList/highscore.aspx
@@ -4,7 +4,7 @@
     <asp:GridView ID="grdScores" runat="server" AutoGenerateColumns="false" DataKeyNames="username"
          CssClass="table table-striped table-hover"
          AllowPaging="true" PageSize="3"
-         AllowSorting="true" OnRowDataBound="grdScores_RowDataBound">
+         AllowSorting="true">
 
         <Columns>
             <asp:BoundField DataField="username" HeaderText="User" SortExpression="User" />
--- ToDoList/highscore.aspx.cs
+++ ToDoList/highscore.aspx.cs
@@ -5,13 +5,39 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//reference the EF models
+using ToDoList.Models;
+using System.Web.ModelBinding;
+
 namespace ToDoList
 {
     public partial class highscore : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                GetPoints();
+            }
         }
+
+        protected void GetPoints()
+        {
+            using (ToDoDBase db = new ToDoDBase())
+            {
+
+                String user = User.Identity.Name;
+
+                //query the points table using EF and LINQ
+                var points = from s in db.pointTrackers
+                            select s;
+
+                //bind the result to the gridview
+                grdScores.DataSource = points.ToList();
+                grdScores.DataBind();
+
+            }
+        }
+
     }
 }
